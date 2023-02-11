3fb4250040cb50b149f55df77b5f8ca41b1b7aec
Justin Wesley <jdubedition@gmail.com>
2023-02-11 19:04:12 +0000

    add utilities directory for nodejs dev container

---
+++ utilities/use-omz.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pip install diffusers transformers accelerate scipy safetensors
+pip install torch torchvision torchaudio
+pip install pillow
+
+echo "zsh does not exist."
+apt update
+# Install zsh and less, because zsh is used by oh-my-zsh and less is used by git
+apt install zsh less curl git -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+/usr/bin/zsh
